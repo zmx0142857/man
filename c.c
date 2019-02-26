1fb267ef99dea232f57b1089351861b75a1cb85c
zmx <zmx0142857@qq.com>
2019-02-26 18:39:37 +0800

    edit c.c dict-en.txt mv latex.tex -> latex/latex-example.tex

--- c.c
+++ c.c
@@ -6,6 +6,24 @@
  *	-Dxxx=???			define a macro named xxx, valued ???
  */
 
+#include <stdio.h>
+
+FILE *fopen(const char *filename, const char *mode);
+
+	// mode
+	//	"r"				read only, file must exist
+	//	"w"				clear the file and ready to write,
+	//					new file is created if not exist.
+	//	"a"				append: write to the end of a file,
+	//					new file is created if not exist.
+
+	// example
+	FILE *file = fopen("myfile.txt", "w");
+	if (file) {
+		fputs("hello world", file);
+		fclose(file);
+	}
+
 #include <string.h>
 
 void *memcpy(dest, src, size);
@@ -15,7 +33,10 @@ void memset(ptr, value, size);
 
 void *malloc(size);
 
+	// example
 	int *arr = (int*)malloc(sizeof(int)*n);
 
 void free(ptr);
 int atoi(str);
+
+#include <ctype.h>
---
+++ latex/hello.c
@@ -0,0 +1,11 @@
+/* 
+ * output hello world string
+ */
+#include <stdio.h>
+int main()
+{
+	// also support chinese characters
+	printf("hello world 你好 世界\n");
+	return 0;
+}
+

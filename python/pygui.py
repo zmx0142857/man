8c1121458d2442d3d8aef74518cd24afe57c8017
zmx <zmx0142857@qq.com>
2019-03-16 09:45:14 +0800

    rm latex/raw/aux toc log ... mv python.py -> python/ add python/pygui.py edit scheme.ss

---
+++ python/pygui.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()         # add widget to its parent
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.promptLabel = Label(self, text='What\'s your name?')
+        self.promptLabel.pack()
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
+
+app = Application()
+app.master.title('Hello World')
+app.mainloop()

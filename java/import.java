807effa1ea1cfe0fd7e5d5c9a9ed5ae901924f52
zmx <zmx0142857@qq.com>
2019-03-16 10:21:17 +0800

    edit android.man add java/import.java

---
+++ java/import.java
@@ -0,0 +1,6 @@
+//java.lang is loaded automatically and is unnecessary to import
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;

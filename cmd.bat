5206294a37ea9fbfc135d55f4b6276b6980844d5
zmx <zmx0142857@qq.com>
2019-03-31 12:12:44 +0800

    edit cmd.bat windows.man

--- cmd.bat
+++ cmd.bat
@@ -40,13 +40,15 @@
 cmd (command processor, 命令处理器)
 
 	windows 的 batch 脚本解释器. 在 dos 时代, 它是 command.com,
-	在 windows 中则是 %system%/cmd.exe.
+	在 windows 中则是 cmd.exe (%systemroot%/system32/cmd.exe).
 
 控制台 (console)
 
 	cmd 运行时打开的黑窗口. 是 IO (=输入输出) 设备之一.
 	cmd 中的常见 IO 设备有 con 控制台, prn 打印机, nul 空设备,
-	当然文件也可以是 IO 的对象.
+	其中空设备表示丢弃这些 IO 信息, 可以把 nul 理解为无底洞.
+	当然文件也可以是 IO 的对象. 因此, con, prn, nul 是 windows 的保留名,
+	给文件命名时, 不可以取这些名字.
 
 命令
 	
@@ -79,8 +81,9 @@ cmd (command processor, 命令处理器)
 
 扇区
 
-	又叫分区. 同一块磁盘可以有多个分区. 跨分区的文件访问存在一些限制.
-	一般不宜设定过多的分区, 一个系统分区和一个用户文件分区已经足够.
+	又叫分区或驱动器 (driver). 同一块物理磁盘可以有多个分区.
+	跨分区的文件访问存在一些限制.  一般不宜设定过多的分区,
+	一个系统分区和一个用户文件分区已经足够.
 
 ---- 入门操作 -------------------------------------------------------
 
@@ -137,7 +140,6 @@ dir [option] [path/file]
 
 	dir c*.*			列出以 "c" 开头的所有文件
 	dir *.*				列出所有文件, 不包括隐藏, 系统文件
-	dir #				同上
 	dir ?.exe			列出所有文件名是一个字符的 .exe 文件
 
 pushd
@@ -150,6 +152,23 @@ popd
 	pushd c:\bar		保存当前目录后, 切换到 c:\bar
 	popd				回到刚才保存的目录 d:\foo
 
+ctrl-c
+
+	用于中止命令的运行. 比如
+
+	ping -t 127.0.0.1
+	^C
+
+	就结束了 ping 的运行.
+
+ctrl-z enter
+
+	用于输入文件结束符 (EOF). 如
+
+	type con
+
+	向控制台随意输入一些内容后, ctrl-z 并回车以结束输入.
+
 ---- 装点操作 -------------------------------------------------------
 
 cls
@@ -160,7 +179,7 @@ pause
 
 	运行该命令时, 将显示下面的消息: 
 
-		请按任意键继续 . . .
+		请按任意键继续. . .
 
 	一般用于看清楚屏幕上显示的内容, 避免图形界面下程序 "一闪而过".
 
@@ -173,20 +192,42 @@ title [newtitle]
 color [bg][fg]
 
 	设置 cmd 窗口的颜色, 有 0-9a-f 共 16 色.
+	缺省参数时, 使用默认颜色.
 
-	color /???
+	0	深黑				8	浅黑
+	1	深蓝				9	浅蓝
+	2	深绿				a	浅绿
+	3	深青				b	浅青
+	4	深红				c	浅红
+	5	深紫				d	浅紫
+	6	深黄				e	浅黄
+	7	深白				f	浅白
 
-prompt
+	color fc	浅白色背景, 浅红色前景
+	color f0	白底黑字, 适合投影仪
 
-	更改命令提示符号
+prompt [text]
 
-	prompt /???
+	更改命令提示符号. 缺省参数时, 变回默认提示符.
+
+	$a	&           $n	当前分区 
+	$b	|           $p	当前路径
+	$c	(           $q	=
+	$d	当前日期    $s	空格
+	$e	escape      $t	当前时间
+	$f	)           $v	windows 版本
+	%g	>           $_	换行符
+	$h	backspace   $$	$           
+	$l	<
 
 doskey [alias]=[command]
 
 	命令别名
 
-	doskey vi=vim $*		vi 是 vim 的别名
+	doskey cd=cd /d							为 cd 加上默认的 /d 选项
+	doskey ls=dir /b						模仿 unix 的命令名
+	doskey vi=vim $*						vi 是 vim 的别名
+	doskey hash=certutil -hashfile $1 $2	方便计算 hash, $1 $2 为参数
 
 chcp [codepage]
 	
@@ -253,6 +294,8 @@ find "string" [file]
 	find "abc" file		在 file 中查找含 "abc" 的行;
 						找不到则置 %errorlevel%=1
 
+	ipconfig | find "IPv6"	在 ipconfig 的输出中寻找含 IPv6 的行
+
 certutil -hashfile [filename] [md5|sha1|sha256]
 
 	外部命令, 计算文件的 hash 值. 通常用于检查下载的文件的完整性.
@@ -274,8 +317,7 @@ move [option] [source-file] [target-path/file]
 	内部命令, 移动文件. 相当于 unix 的 mv.
 	默认情况下, 不移动隐藏, 系统文件.
 	省略目标文件名时, 使用原文件的名字.
-
-	警告! 目标目录下的同名文件将被覆盖.
+	目标目录下存在同名文件会询问是否覆盖.
 
 copy [option] [source-file...] [target-path/file]
 	
@@ -283,8 +325,7 @@ copy [option] [source-file...] [target-path/file]
 	默认情况下, 不拷贝隐藏, 系统文件.
 	省略目标文件名时, 使用原文件的名字.
 	完全省略目标路径时, 复制到当前目录. 系统会自动重命名.
-
-	警告! 目标目录下的同名文件将被覆盖.
+	目标目录下存在同名文件会询问是否覆盖.
 
 	copy c:\test.txt d:\			复制 c:\test.txt 文件到 d:\
 	copy c:\test.txt d:\test.bak	复制 c:\test.txt 文件到 d:\,
@@ -308,6 +349,7 @@ xcopy [option] [source-path/file...] [target-path]
 	外部命令, 是 copy 的强化版, 可以拷贝整个目录结构.
 	默认不拷贝隐藏, 系统文件.
 
+	/b						复制快捷方式的目标而不是快捷方式
 	/e						一并拷贝空目录
 	/i						如果目标目录不存在则自动新建
 	/s						一并拷贝子目录
@@ -347,6 +389,7 @@ del [option] [file]
 	注意! 用 rd 和 del 删除的文件和目录都不会进入回收站.
 
 	/a					(all) 包括受保护的文件, 即隐藏, 只读, 系统文件
+	/f					(force) 强制删除只读文件
 	/p					(prompt) 删除前总是询问
 	/q					(quiet) 删除前总是不询问
 	/s					(subdir) 一并删除子目录下的文件
@@ -357,7 +400,7 @@ del [option] [file]
 								(也是一干二净)
 undelete [option] [file]
 
-	外部命令, 恢复误删的文件.
+	外部命令, 恢复误删的文件. (警告: win 10 不存在此命令)
 
 	/dos				根据目录中残留的记录来恢复文件.  文件被删除时,
 						文件名第一个字节会被改为E5, dos 即依据文件开头的
@@ -385,7 +428,7 @@ tree [path]
 
 deltree [path]
 
-	外部命令, 删除目录树, 是一个危险的命令.
+	外部命令, 删除目录树, 是一个危险的命令 (win 10 无此命令).
 	将所指定的目录下的一切删除, 包括受保护的文件.
 
 attrib [file] [attribute]
@@ -441,7 +484,7 @@ ver
 
 msd [option]
 
-	外部命令, 系统报告, 显示硬件和操作系统的一些信息.
+	外部命令, 系统报告, 显示硬件和操作系统的一些信息 (win 10 无此命令).
 
 	/i					(ignore) 不检测硬件
 	/b					(black & white) 以黑白方式启动 msd
@@ -454,10 +497,9 @@ date [mm-dd-yy]
 
 	/t					显示日期
 
-time [hh:mm:ss:xx]
+time [hh:mm]
 
 	内部命令, 设置系统时间.
-	xx 表示百分之一秒.
 	缺省参数时会显示当前时间并提示输入新时间, 如不想修改时间, 直接回车.
 	unix 中, time 是程序计时工具而非查看时间的命令.
 
@@ -467,7 +509,7 @@ time [hh:mm:ss:xx]
 
 mem [option]
 
-	外部命令, 显示当前内存使用状况.
+	外部命令, 显示当前内存使用状况 (win 10 无此命令).
 
 	/c					显示装入常规内存和 cmb 的各文件的长度,
 						并显示已用内存和最大可用空间
@@ -514,8 +556,9 @@ mem [option]
 		copy c:\test.txt f: > nul
 
 	不显示 "文件复制成功" 的提示信息.  f 盘不存在时, 也不显示错误:
+	2> 表示对第二部分输出的重定向 (一般为错误信息)
 
-		copy c:\test.txt f: > nul 2 > nul
+		copy c:\test.txt f: > nul 2> nul
 
 	^ 是转义符号. 生成的文件内容为 ^ >
 
@@ -571,15 +614,6 @@ start
 
 		start explorer d:\
 
-
-	启动 "劲舞团".  不加 start 命令的话, "劲舞团" 运行时,
-	后面会有个黑乎乎的 cmd 窗口. 
-
-	@echo off
-	cd /d %~dp0
-	regedit /s 劲舞团.reg
-	start patcher.exe
-
 call
 
 	调用批处理脚本. 在批处理中, 等子程序完成后才会继续执行剩下的指令.
@@ -599,34 +633,48 @@ call
 	在 C 盘根目录新建 test.bat, 内容如下: 
 
 		@echo off
-		echo %0
-		echo %1
-		echo %2
-		echo %*
+		echo %0			:: 命令名
+		echo %1			:: 第一个参数
+		echo %2			:: 第二个参数
+		echo %*			:: 从第一个参数开始的所有参数
 
-	运行cmd, 输入 c:\test.bat "/a" /b /c /d, 可以看出每个参数的含义. 
+	运行cmd, 输入 c:\test.bat 1 2 3 4, 可以看出每个参数的含义. 
 	修改test.bat内容如下: 
 
 		@echo off
-		echo %1
-		echo %~1
-		echo %0
-		echo %~f0
-		echo %~d0
-		echo %~p0
-		echo %~n0
-		echo %~x0
-		echo %~s0
-		echo %~a0
-		echo %~t0
-		echo %~z0
+		echo %~f0		:: 脚本完整路径
+		echo %~d0		:: 脚本所在分区 (盘符)
+		echo %~p0		:: 脚本路径, 不含盘符与文件名
+		echo %~n0		:: 脚本名, 不含文件名
+		echo %~x0		:: 脚本文件的后缀名
+		echo %~s0		:: 脚本完整路径 (?)
+		echo %~a0		:: 脚本属性
+		echo %~t0		:: 脚本修改日期
+		echo %~z0		:: 脚本文件的大小 (byte)
 
-	再运行 cmd, 输入 c:\test.bat "/a" /b /c /d, 可以参照 call /? 或 for /?
-	看出每个参数的含义. 注意这里可以对文件进行日期比较和大小比较. 
+	这些变量可以连用:
 
-shift
+		echo %~dp0		:: 脚本盘符与路径, 不含文件名
+
+shift [option]
 
 	传递给批处理的参数不止 9 个时, 用以切换参数.
+	将第 10 个参数以及以后的所有参数向前移动, 覆盖第 9 个参数.
+
+	/n					将第 n+1 个参数以及以后的所有参数向前移动,
+						覆盖第 n 个参数
+
+	@echo off
+	echo %*
+	echo %9
+	shift
+	echo %9
+	shift
+	echo %9
+	shift
+	echo %9
+
+	运行: test.bat 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 
 set [variable][=value]
 
@@ -636,14 +684,15 @@ set [variable][=value]
 	set p				显示所有以 p 开头的变量, 没有则置 errorlevel=1
 	set p=hello			设置变量 p, 并赋以字符串值 hello
 	echo %p%			查看变量 p
-	set p=				取消 p 变量
+	set p=				取消 p 变量, 即置 p 为空字符串
 	set /p p=请输入		屏幕上显示 "请输入", 并将输入的字符串赋给变量 p 
 
 	---------------------------------------------------------------------
 	常见系统变量
 
 	systemdrive			系统盘符
-	system				系统目录
+	systemroot			系统目录
+	comspec				cmd 的所在目录
 	appdata				windows 上各应用程序的数据目录
 	path				系统路径, 各路径以半角分号隔开.
 						在 cmd 中输入命令时, 解释器首先在当前目录中搜索
@@ -658,7 +707,8 @@ set [variable][=value]
 	random				0~32767 的随机整数
 	errorlevel			程序返回码, 0 表示程序正常执行, 非正常时为非 0 值
 	cmdextension		当前 cmd 扩展名版本号
-	cmdlin				调用当前 cmd 的原始命令行
+	cmdcmdline			调用当前脚本的原始命令行
+	highestnumanodenumber 当前计算机上的最高 numa 节点号
 	---------------------------------------------------------------------
 
 	字符串切片
@@ -684,6 +734,7 @@ set [variable][=value]
 	set /a p=39/10		舍去小数部分, 39/10=3
 	set /a p=p/10		用 /a 参数时, '=' 后面的变量可以不加 % 直接引用
 	set /a p="1&0"		"与" 运算, 要加引号. 其它支持的运算符参见 set /?
+	set /a p=%random% % 2	抛硬币
 
 	环境变量虽然叫做变量, 但其行为更接近宏, 即字符串的简单替换. 比如,
 	在 if 和 for 语句里, 变量是一次性全部替换的. 在下面的例子中, 读取
@@ -699,11 +750,12 @@ set [variable][=value]
 
 	结果: 
 
-		复读机复读机
+		复读机
+		复读机
 
 	这里的 "替换" 又称为 "扩充" 或 "环境变量扩充". 为使上例的第二个 set
-	语句生效, 可以启用 "延缓环境变量扩充", 并用 ! 而不是 % 来引用变量.
-	另外, 为使 "延缓环境变量" 只在局部有效, 使用了命令 setlocal:
+	语句生效, 可以启用 "延迟环境变量扩充", 并用 ! 而不是 % 来引用变量.
+	另外, 为使 "延迟环境变量" 只在局部有效, 使用了命令 setlocal:
 
 		@echo off
 		:: Enable delayed expansion
@@ -718,9 +770,31 @@ set [variable][=value]
 
 	结果: 
 		
-		复读机必胜
+		复读机
+		必胜
+
+	延迟环境变量扩充同样影响着 for 语句, 尝试运行下面的脚本:
+
+		@echo off
+		set list=
+		for %%i in (*) do set list=%list% %%i
+		echo %list%
+
+	和
+
+		@echo off
+		setlocal enabledelayedexpansion
+		set list=
+		for %%i in (*) do set list=!list! %%i
+		echo %list%
+		endlocal
+
+	可以看到, 第一个版本只输出了最后一个文件名. 这是 list
+	只扩展了一次的缘故.
 
 --- 流程控制 ---------------------------------------------------------
+
+if [condition] [command...]
 
 if [condition] (
 	[command...]
@@ -728,17 +802,54 @@ if [condition] (
 	[command...]
 )
 
-	选择语句
+	选择语句.  condition 可以是以下三种类型:
 
-	if 语句中, 判断 errorlevel 有 3 种写法. 应先判断较大的 errorlevel:
+		str1 == str2
+		errorlevel [n]			errorlevel 大于等于 n
+		exist [file]
 
-		if errorlevel 3 goto ccc
-		if %errorlevel%==2 goto bbb
-		if errorlevel==1 goto aaa
+	为防止参数为空, 一般把判断 %1 == -u 写成 [%1] == [-u].
+	由于 errorlevel n 是指 errorlevel >= n, 故判断 errorlevel 时,
+	一般先判断较大的值.
 
-for
+	not 表示逻辑否:
 
-	循环语句
+		not str1 == str2
+	
+	/i 选项下, if 有更多的可用条件:
+
+		str1 op str2			此时的比较不区分大小写
+		cmdextversion [n]		cmdextversion 大于等于 n
+		defined [variable]
+
+	op 可以是:
+
+		equ			等于
+		neq			不等于
+		lss			小于
+		leq			小于或等于
+		gtr			大于
+		geq			大于或等于
+
+for [%v] in ([set]) do [command...]
+
+	循环语句.
+
+		%v			单个字母的参数, 区分大小写
+		[set]		一个或一组文件, 支持通配符
+
+	在脚本中使用 for 语句时, 应该使用 %%v 而不是 %v
+
+	for /d ...		若 [set] 中含有通配符, 则只匹配目录名而不匹配文件名
+	for /r [path]	递归搜索 [path] 下的子目录, 缺省使用当前目录
+	for /l %v in (start,step,end) ...
+					在数字序列中迭代, 如 (1,1,3) 是 1, 2, 3;
+					(3,-1,1) 是 3, 2, 1.
+
+	分析 file 的每一行, 忽略以分号打头的行, 将每行的第 2, 3 个 token 传给
+	for 循环体, 用逗号和/或空格分隔 tokens.  %i 代表第 2 个 token,
+	%j 代表第 3 个 token, %k 代表剩余所有 tokens.
+	for /f "eol=; tokens=2,3* delims=, " %i in (file) do @echo %i %j %k
 
 goto
 
@@ -859,7 +970,7 @@ subst [disk] [path]
 	subst					显示所有映射
 	subst z: /d				取消映射
 
-defrag disk [option]
+defrag [disk] [option]
 
 	外部命令
 	整理磁盘, 消除磁盘碎片
@@ -867,7 +978,7 @@ defrag disk [option]
 	/f					调整文件在磁盘上的安排, 确保文件间无空隙,
 						从而节省磁盘空间, 提高读写效率.
 
-chkdsk path [option]
+chkdsk [path] [option]
 
 	检查磁盘, 外部命令.
 	显示磁盘状态/内存状态和指定路径下指定文件的不连续数目.
@@ -875,7 +986,7 @@ chkdsk path [option]
 	/f					纠正在磁盘上发现的逻辑错误
 	/v					显示盘上所有文件和路径
 
-scandisk disks
+scandisk [disks]
 
 	检查/修复磁盘, 外部命令
 	检测磁盘的 fat 表, 目录结构, 文件系统等
@@ -884,20 +995,20 @@ scandisk disks
 
 	/all				修复所有磁盘
 
-diskcopy disk1 disk2
+diskcopy [disk1] [disk2]
 	
 	整盘复制, 外部命令.
 	复制软盘上的所有信息, 目标软盘将自动被格式化, 原有文件会全部丢失.
 	如果是单驱动器复制, 系统会适时提示更换软盘.
 	操作时务必分清源盘与目标盘.
 
-sys disk
+sys [disk]
 	
 	复制系统, 外部命令
 	将当前驱动器上的 DOS 系统文件 io.sys, msdos.sys 和 command.com
 	传送到指定的驱动器上. 若目标磁盘剩余空间不足以存放系统文件, 则报错.
 
-format disk
+format [disk]
 
 	格式化, 外部命令
 	对磁盘进行格式化, 划分磁道和扇区; 同时检查坏道,
@@ -916,7 +1027,7 @@ format disk
 	/u					(unrecoverable) 不可逆格式化.
 						以 F6 覆盖分区的每一字节, 破坏原有的一切数据.
 
-unformat disk
+unformat [disk]
 
 	恢复格式化, 外部命令
 	用于从误格式化的磁盘中恢复数据. 若格式化时没有使用 /u 参数,
@@ -953,3 +1064,5 @@ diskpart
 	外部命令, 硬盘分区.
 
 ========================================================================
+
+windows 如何 echo 多行内容

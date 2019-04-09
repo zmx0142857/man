5ebb0667e67d2701571c44f9b2a7f00615c0f6bb
zmx <zmx0142857@qq.com>
2019-04-09 23:19:57 +0800

    edit cpp.cpp edit latex-example: add merging pdf add jquery.js

--- cpp.cpp
+++ cpp.cpp
@@ -1,5 +1,5 @@
 // force to use c++11
-#if __cplusplus < 201101
+#if __cplusplus < 201103
 #error "Require c++11 or later"
 #endif
 
@@ -13,3 +13,18 @@ string substr(size_type pos=0, size_type count=npos) const;
 
 // enable conversion to int
 operator int() const { ... }
+
+#include <iostream>
+#include <gmpxx.h> // -lgmpxx -lgmp
+using namespace std;
+
+int main()
+{
+	mpz_class a, b, c;
+	a = 1234;
+	b = "-5678";
+	c = a + b;
+	cout << "sum is " << c << "\n";
+	cout << "absolute value is " << abs(c) << "\n";
+	return 0;
+}
---
+++ jquery.js
@@ -0,0 +1,89 @@
+// ---- 安装 ------------------------------------------------------------
+
+方法 1. 从 jquery.com 下载 jQuery 库
+
+<head>
+	<script src="js/jquery-1.10.2.min.js"></script>
+</head>
+
+方法 2. 从 cdn 中载入 jQuery (任选其一)
+
+<head>
+	<!-- Staticfile -->
+	<script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+	<!-- 百度 -->
+	<script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+	<!-- 又拍云 -->
+	<script src="https://upcdn.b0.upaiyun.com/libs/jquery/jquery-2.0.2.min.js"></script>
+	<!-- 新浪 -->
+	<script src="https://lib.sinaapp.com/js/jquery/2.0.2/jquery-2.0.2.min.js"></script>
+	<!-- google -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<!-- microsoft -->
+	<script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.min.js"></script>
+</head>
+
+使用 CDN 的优势是，许多用户在访问其他站点时，已经从百度、又拍云、新浪、谷歌或微软加载过 jQuery。所以结果是，当他们访问您的站点时，会从缓存中加载 jQuery，这样可以减少加载时间。同时，大多数 CDN 都可以确保当用户向其请求文件时，会从离用户最近的服务器上返回响应，这样也可以提高加载速度。
+
+// ---- 语法 -----------------------------------------------------------
+
+通过 jQuery，您可以选取（查询，query） HTML 元素，并对它们执行"操作"（actions）。
+
+基础语法： $(selector).action()
+
+    美元符号定义 jQuery
+    选择符（selector）"查询"和"查找" HTML 元素
+    jQuery 的 action() 执行对元素的操作
+
+实例:
+
+    $(this).hide() - 隐藏当前元素
+
+    $("p").hide() - 隐藏所有 <p> 元素
+
+    $("p.test").hide() - 隐藏所有 class="test" 的 <p> 元素
+
+    $("#test").hide() - 隐藏所有 id="test" 的元素
+
+// ---- 文档就绪事件 ---------------------------------------------------
+
+您也许已经注意到在我们的实例中的所有 jQuery 函数位于一个 document ready 函数中：
+
+$(document).ready(function(){
+ 
+   // 开始写 jQuery 代码...
+ 
+});
+
+这是为了防止文档在完全加载（就绪）之前运行 jQuery 代码，即在 DOM 加载完成后才可以对 DOM 进行操作。
+
+如果在文档没有完全加载之前就运行函数，操作可能失败。
+
+上面代码的简写：
+
+$(function(){
+ 
+   // 开始写 jQuery 代码...
+ 
+});
+
+jQuery 入口函数:
+
+$(document).ready(function(){
+    // 执行代码
+});
+或者
+$(function(){
+    // 执行代码
+});
+
+JavaScript 入口函数:
+
+window.onload = function () {
+    // 执行代码
+}
+
+jQuery 入口函数与 JavaScript 入口函数的区别：
+
+jQuery 的入口函数是在 html 所有标签(DOM)都加载之后，就会去执行。
+JavaScript 的 window.onload 事件是等到所有内容，包括外部图片之类的文件加载完后，才会执行。

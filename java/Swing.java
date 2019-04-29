85882545f6a5dc2551ab31623769ec274008dcf7
zmx <zmx0142857@qq.com>
2019-04-29 11:10:48 +0800

    add more java-exmples add rubik-cube.jl

--- java/Swing.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class Swing {
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Welcome to Java Programming!");
-		System.exit(0);
-	}
-}
-

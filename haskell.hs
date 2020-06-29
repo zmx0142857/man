7567d7dbc85fdafccbc410c877ed93f6e0d3e3a0
zmx <892298182@qq.com>
2020-06-30 00:12:21 +0800

    edit convert.man haskell.hs vim.man mv ime.man -> ime/ime.man

--- haskell.hs
+++ haskell.hs
@@ -145,3 +145,7 @@ oddSquareSum =
     let oddSquares = filter odd $ map (^2) [1..]
         belowLimit = takeWhile (<10000) oddSquares
     in sum belowLimit
+
+
+import Data.List
+import Data.Function
